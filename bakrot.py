2e135f3a1d512af6f3de49e9137f9dad118dd101
Bill Melvin <bill@billmelvin.com>
2020-08-17 11:21:20 -0400

    Initial commit for Backup Rotation calculator.

---
+++ bakrot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from datetime import date
+from datetime import timedelta
+
+
+#start_date = date(2020,7,4)
+#end_date = date(2022,1,1)
+#for w in range(0,100):
+#    d = start_date + timedelta(weeks=w)
+#    print(d)
+
+
+level_1_set = ['','','','','','','']
+level_1_size = 7
+for i in range(level_1_size):
+  print(level_1_set[i])
+
