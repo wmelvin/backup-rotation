ab7a347efdb2e09479152b3f4b8a2435ffd08ce0
Bill Melvin <bill@billmelvin.com>
2020-08-18 10:34:36 -0400

    Rename classes to DrivePool and RotationLevel. Add drive_pool attribute to RotationLevel.

--- bakrot.py
+++ bakrot.py
@@ -3,7 +3,7 @@
 from datetime import date
 from datetime import timedelta
 
-class drive_pool():
+class DrivePool():
     def __init__(self):
         self.drivepool = []
         self.lastdrive = 0
@@ -13,23 +13,24 @@ class drive_pool():
         if len(self.drivepool) == 0:
             self.lastdrive = self.lastdrive + 1
             n = self.lastdrive
-            print(f"drive_pool.get_next_drive()={n} new drive")
+            print(f"DrivePool.get_next_drive: {n} new")
         else:
             n = self.drivepool.pop()
-            print(f"drive_pool.get_next_drive()={n} from pool")
+            print(f"DrivePool.get_next_drive: {n} from pool")
         return n
             
     def add_drive(self, drive_number):
         if 0 < drive_number:
-            print(f"drive_pool.add_drive({drive_number})")
+            print(f"DrivePool.add_drive: {drive_number} added to pool")
             self.drivepool.append(drive_number)
 
 
-class rotation_level():
-    def __init__(self, level, num_drives, usage_interval, lower_level_obj):
+class RotationLevel():
+    def __init__(self, level, num_drives, usage_interval, drive_pool, lower_level_obj):
         self.level = level
         self.num_drives = num_drives
         self.usage_interval = usage_interval
+        self.drive_pool = drive_pool
         self.next_level_down = lower_level_obj
         self.cycle_num = -1
         self.cycle_date = -1
@@ -41,13 +42,9 @@ class rotation_level():
     def start_cycle(self, cycle_num, cycle_date):
         self.cycle_num = cycle_num
         self.cycle_date = cycle_date
-
         self.prevs = [[self.drives[x][0], self.drives[x][1]] for x in range(self.num_drives)]
-
         self.cycle_index = (self.usage_cycle(cycle_num) % self.num_drives)           
-
         self.in_cycle = ((cycle_num + 1) % self.usage_interval == 0)
-        
         print(f"L{self.level} start_cycle {cycle_num}, date={cycle_date}, index={self.cycle_index}, in_cycle={self.in_cycle}")
 
         
@@ -66,41 +63,34 @@ class rotation_level():
     # def is_used(self, cycle):
     #     return ((cycle + 1) % self.usage_interval == 0)
 
-    def pull_drive(self, pool, cycle):
+    def pull_drive(self):
         n = int(self.drives[self.cycle_index][0])                
         if 0 < n:      
             d = self.drives[self.cycle_index][1]      
             self.drives[self.cycle_index][0] = (n * -1)            
-            print(f"pull_drive: level={self.level}, cycle={cycle}, index={self.cycle_index}, drive={n}, date={d}")
+            print(f"L{self.level} pull_drive: cycle={self.cycle_num}, index={self.cycle_index}, drive={n}, date={d}")
             return [n, d]
         else:
-            print(f"pull_drive: level={self.level}, cycle={cycle}, index={self.cycle_index}, No drive to pull")
+            print(f"L{self.level} pull_drive: cycle={self.cycle_num}, index={self.cycle_index}, No drive to pull")
             return [0,0]
             
-    # def pull_from(self, other_level, pool, cycle):
-    #     if self.is_used(cycle):
-    #         print(f"pull_from: level={self.level}, cycle={cycle}, index={self.cycle_index}")            
-    #         self.drives[self.cycle_index] = other_level.pull_drive(pool, cycle)
-    def pull_from(self, pool, cycle):
-        #if self.is_used(cycle):
+    def pull_from_lower_level(self):
         if self.in_cycle:
-            print(f"pull_from: level={self.level}, cycle={cycle}, index={self.cycle_index}")            
-            self.drives[self.cycle_index] = self.next_level_down.pull_drive(pool, cycle)
+            print(f"L{self.level} pull_from:  cycle={self.cycle_num}, index={self.cycle_index}")            
+            self.drives[self.cycle_index] = self.next_level_down.pull_drive()
                                     
-    def free_drive(self, pool, cycle):
-        #if self.is_used(cycle):
+    def free_drive(self):
         if self.in_cycle:
             n = int(self.drives[self.cycle_index][0])
             if 0 < n:
-                print(f"free_drive: level={self.level}, cycle={cycle}, index={self.cycle_index}")            
-                pool.add_drive(self.drives[self.cycle_index][0])                        
+                print(f"L{self.level} free_drive: cycle={self.cycle_num}, index={self.cycle_index}")            
+                self.drive_pool.add_drive(self.drives[self.cycle_index][0])                        
                 self.drives[self.cycle_index][0] = n * -1
 
-    def next_drive(self, pool, cycle):
-        #if self.is_used(cycle):
+    def next_drive(self):
         if self.in_cycle:
-            print(f"next_drive: level={self.level}, cycle={cycle}, index={self.cycle_index}")            
-            self.drives[self.cycle_index][0] = pool.get_next_drive()
+            print(f"L{self.level} next_drive: cycle={self.cycle_num}, index={self.cycle_index}")            
+            self.drives[self.cycle_index][0] = self.drive_pool.get_next_drive()
             self.drives[self.cycle_index][1] = self.cycle_date
             
             
@@ -131,11 +121,11 @@ class rotation_level():
 start_date = date(2020,7,4)
 n_weeks = 10
 
-dp = drive_pool()
+dp = DrivePool()
 
-l1 = rotation_level(1, 4, 1, None)
-l2 = rotation_level(2, 3, 2, l1)
-l3 = rotation_level(3, 2, 4, l2)
+l1 = RotationLevel(1, 4, 1, dp, None)
+l2 = RotationLevel(2, 3, 2, dp, l1)
+l3 = RotationLevel(3, 2, 4, dp, l2)
 
 
 if False:
@@ -160,34 +150,34 @@ if False:
         s += f",{l3.cycle_num},{l3.cycle_index},{l3.in_cycle:1}"
         print(s)
 
+
 if True:
     out_list = []
     s = f"iter,date{l1.csv_head()},.{l2.csv_head()},.{l3.csv_head()},notes"
     out_list += f"{s}\n"
     out_list2 = out_list.copy()
     
-    for w in range(n_weeks):
-        d = start_date + timedelta(weeks=w)
+    for week_num in range(n_weeks):
+        week_date = start_date + timedelta(weeks=week_num)
         
-        l1.start_cycle(w, d)
-        l2.start_cycle(w, d)
-        l3.start_cycle(w, d)
+        l1.start_cycle(week_num, week_date)
+        l2.start_cycle(week_num, week_date)
+        l3.start_cycle(week_num, week_date)
         
-        l3.pull_from(dp, w)
+        l3.pull_from_lower_level()        
+        l2.pull_from_lower_level()
         
-        l2.pull_from(dp, w)
+        l1.free_drive()
         
-        l1.free_drive(dp, w)
-        
-        s = f"{w},{d}{l1.csv_data()},{l2.csv_data()},{l3.csv_data()},before next drive"
+        s = f"{week_num},{week_date}{l1.csv_data()},{l2.csv_data()},{l3.csv_data()},before next drive"
         out_list2 += f"{s}\n"
         
-        l1.next_drive(dp, w)
+        l1.next_drive()
         
-        s = f"{w},{d}{l1.csv_data()},{l2.csv_data()},{l3.csv_data()},after next drive"
+        s = f"{week_num},{week_date}{l1.csv_data()},{l2.csv_data()},{l3.csv_data()},after next drive"
         out_list2 += f"{s}\n"
 
-        s = f"{w},{d}{l1.csv_diff()},{l2.csv_diff()},{l3.csv_diff()}"
+        s = f"{week_num},{week_date}{l1.csv_diff()},{l2.csv_diff()},{l3.csv_diff()}"
         out_list += f"{s}\n"
 
     with open('bakrot_output.csv', 'w') as out_file:
@@ -195,6 +185,7 @@ if True:
 
     with open('bakrot_output_data.csv', 'w') as out_file:
         out_file.writelines(out_list2)
+
 
 if False:
     l1.list_drives()
